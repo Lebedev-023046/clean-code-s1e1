456f6f7d6edc577a702d128b6774cfbc91a0cef2
lebedev-023046 <lebedev.023046@gmail.com>
2022-06-02 17:48:09 +0300

    fix: add BEM-styles in js according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -7,17 +7,17 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new__task");//Add a new task.
 var addButton=document.querySelectorAll(".btn")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomp-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("comp-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incomp__tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("comp__tasks");//completed-tasks
 
 
 //New task list item
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("article");
-    listItem.classList.add('list-item')
+    listItem.classList.add('list__item')
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
